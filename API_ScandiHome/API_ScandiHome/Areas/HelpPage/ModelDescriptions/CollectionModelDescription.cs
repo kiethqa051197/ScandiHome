ff05c6aaa80e7bc4b5b8bab9849265344871c0be
Kiet Ha <hqakiet@tma.com.vn>
2022-09-19 16:44:52 +0700

    init api project

---
+++ API_ScandiHome/API_ScandiHome/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace API_ScandiHome.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
