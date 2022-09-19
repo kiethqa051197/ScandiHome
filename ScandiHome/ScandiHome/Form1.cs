0a2a25503bcff610ba0c5803b7394a71fe6df8b3
Kiet Ha <hqakiet@tma.com.vn>
2022-09-19 17:39:51 +0700

    read file ini

--- ScandiHome/ScandiHome/Form1.cs
+++ ScandiHome/ScandiHome/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,14 @@ namespace ScandiHome
         public Form1()
         {
             InitializeComponent();
+
+            var path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+            ReadWriteINIfile readWriteINIfile = new ReadWriteINIfile(Path.Combine(path, "AppConfig.INI"));
+
+            textBox1.Text = readWriteINIfile.ReadINI("Connection", "DBConnection");
+            textBox2.Text = readWriteINIfile.ReadINI("Connection", "User");
+            textBox3.Text = readWriteINIfile.ReadINI("Connection", "Pass");
         }
     }
 }
---
+++ ScandiHome/ScandiHome/ReadWriteINIfile.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ScandiHome
+{
+	public class ReadWriteINIfile
+	{
+		[DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+		private static extern long WritePrivateProfileString(string name, string key, string val, string filePath);
+		[DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+		private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+		public string path;
+
+		public ReadWriteINIfile(string inipath)
+		{
+			path = inipath;
+		}
+		public void WriteINI(string name, string key, string value)
+		{
+			WritePrivateProfileString(name, key, value, this.path);
+		}
+		public string ReadINI(string name, string key)
+		{
+			StringBuilder sb = new StringBuilder(255);
+			int ini = GetPrivateProfileString(name, key, "", sb, 255, this.path);
+			return sb.ToString();
+		}
+	}
+}
