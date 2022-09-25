ff0af6e35eeb136d98cf8631c95217b0fc94f801
Anh Kiet <kiethqa051197@gmail.com>
2022-09-25 22:15:28 +0700

    add some form

---
+++ ScandiHome/ScandiHome/System/frmLogin.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace ScandiHome
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.chk_Remember = new DevExpress.XtraEditors.CheckEdit();
+            this.txtPass = new DevExpress.XtraEditors.TextEdit();
+            this.lblPass = new DevExpress.XtraEditors.LabelControl();
+            this.txtUserName = new DevExpress.XtraEditors.TextEdit();
+            this.lblUserName = new DevExpress.XtraEditors.LabelControl();
+            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            this.btnLogin = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.chk_Remember.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPass.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUserName.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chk_Remember
+            // 
+            this.chk_Remember.Location = new System.Drawing.Point(132, 71);
+            this.chk_Remember.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chk_Remember.Name = "chk_Remember";
+            this.chk_Remember.Properties.Caption = "Ghi nhớ đăng nhập";
+            this.chk_Remember.Size = new System.Drawing.Size(149, 24);
+            this.chk_Remember.TabIndex = 4;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPass.Location = new System.Drawing.Point(132, 41);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Properties.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(251, 22);
+            this.txtPass.TabIndex = 3;
+            this.txtPass.Tag = "";
+            this.txtPass.ToolTip = "Mật khẩu";
+            // 
+            // lblPass
+            // 
+            this.lblPass.Location = new System.Drawing.Point(58, 44);
+            this.lblPass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(56, 17);
+            this.lblPass.TabIndex = 2;
+            this.lblPass.Text = "Mật khẩu";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUserName.Location = new System.Drawing.Point(132, 12);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(251, 22);
+            this.txtUserName.TabIndex = 1;
+            this.txtUserName.Tag = "";
+            this.txtUserName.ToolTip = "Tên đăng nhập";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.Location = new System.Drawing.Point(21, 14);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(93, 17);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "Tên đăng nhập";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Appearance.Options.UseFont = true;
+            this.btnCancel.AutoSize = true;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnCancel.ImageOptions.SvgImage")));
+            this.btnCancel.ImageOptions.SvgImageSize = new System.Drawing.Size(20, 20);
+            this.btnCancel.Location = new System.Drawing.Point(297, 105);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Padding = new System.Windows.Forms.Padding(3);
+            this.btnCancel.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.btnCancel.Size = new System.Drawing.Size(86, 35);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Đó&ng";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Appearance.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Appearance.Options.UseFont = true;
+            this.btnLogin.AutoSize = true;
+            this.btnLogin.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnLogin.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnLogin.ImageOptions.SvgImage")));
+            this.btnLogin.ImageOptions.SvgImageSize = new System.Drawing.Size(20, 20);
+            this.btnLogin.Location = new System.Drawing.Point(169, 105);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Padding = new System.Windows.Forms.Padding(3);
+            this.btnLogin.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.btnLogin.Size = new System.Drawing.Size(122, 35);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "&Đăng nhập";
+            // 
+            // frmLogin
+            // 
+            this.ActiveGlowColor = System.Drawing.Color.Blue;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(418, 160);
+            this.Controls.Add(this.chk_Remember);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLogin);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(420, 200);
+            this.MinimumSize = new System.Drawing.Size(420, 200);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            ((System.ComponentModel.ISupportInitialize)(this.chk_Remember.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPass.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUserName.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.CheckEdit chk_Remember;
+        private DevExpress.XtraEditors.TextEdit txtPass;
+        private DevExpress.XtraEditors.LabelControl lblPass;
+        private DevExpress.XtraEditors.TextEdit txtUserName;
+        private DevExpress.XtraEditors.LabelControl lblUserName;
+        private DevExpress.XtraEditors.SimpleButton btnCancel;
+        private DevExpress.XtraEditors.SimpleButton btnLogin;
+    }
+}
